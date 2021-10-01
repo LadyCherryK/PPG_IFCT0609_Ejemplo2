b8af266b0d573cb878c187ab9186ab02c52e68a2
tamsu <tamsu@LAPTOP-KH477OF4>
2021-10-01 19:11:06 +0200

    Nobita es tontaco

--- IFCT0609_GitHub2/src/ejemplo02/Ejercicio1_2GitHub.java
+++ IFCT0609_GitHub2/src/ejemplo02/Ejercicio1_2GitHub.java
@@ -5,6 +5,7 @@ public class Ejercicio1_2GitHub {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba para commitear y hacer push con GitHub");
 		System.out.println("Espero que Tamsuey este feliz con mi archivo .java :)");
+		System.out.println("Estoy feliz");
 
 	}
 
