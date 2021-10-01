63068d81242a51e78be7c0b05dd213b275d8ac8c
misa_ <misa_@192.168.1.173>
2021-10-01 16:38:14 +0200

    Como dice el profe: vamos a cacharrear <3

---
+++ IFCT0609_GitHub2/src/ejemplo02/Ejercicio1_2GitHub.java
@@ -0,0 +1,11 @@
+package ejemplo02;
+
+public class Ejercicio1_2GitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es una prueba para commitear y hacer push con GitHub");
+		System.out.println("Espero que Tamsuey este feliz con mi archivo .java :)");
+
+	}
+
+}
---
+++ IFCT0609_GitHub2/src/module-info.java
@@ -0,0 +1,3 @@
+module IFCT0609_GitHub2 {
+	//segunda prueba de ejemplo del ejercicio de GitHub-Eclipse
+}
